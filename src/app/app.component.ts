039ff37b3dba0e4f1afd977bd36ebdaa9521985e
gbaksa22 <gbaksa22@gmail.com>
2024-05-03 09:28:28 -0400

    updates item.isComplete on click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  items = [
+  items: WishItem[] = [
     new WishItem('To Learn Angular'),
     new WishItem('Go grocery shopping', true),
     new WishItem('Make dinner')
   ]
   title = 'wishlist';
+
+  toggleItem(item : WishItem) {
+    item.isComplete = !item.isComplete;
+    console.log(item);
+  }
 }
