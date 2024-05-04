4af35dcc33562616513ce4398bb0fca98e3ddfb5
gbaksa22 <gbaksa22@gmail.com>
2024-05-04 10:38:49 -0400

    getter for visible items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,11 @@ import { WishItem } from '../shared/models/wishItem';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+const filters = [
+  (item : WishItem) => item,
+  (item : WishItem) => !item.isComplete,
+  (item : WishItem) => item.isComplete
+]
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -15,14 +20,20 @@ import { FormsModule } from '@angular/forms';
 })
 export class AppComponent {
   items: WishItem[] = [
-    // new WishItem('To Learn Angular'),
-    // new WishItem('Go grocery shopping', true),
-    // new WishItem('Make dinner')
+    new WishItem('To Learn Angular'),
+    new WishItem('Go grocery shopping', true),
+    new WishItem('Make dinner')
   ]
   
+  listFilter : any = '0';
+
   newWishText = '';
 
   title = 'wishlist';
+
+  get visibleItems() : WishItem[] {
+    return this.items.filter(filters[this.listFilter]);
+  }
 
   addNewWish() {
     //add new wish to the items array
