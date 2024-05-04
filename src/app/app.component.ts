44a78323bea4f8925e717807c2bdd7f7a3019ec3
gbaksa22 <gbaksa22@gmail.com>
2024-05-04 08:58:51 -0400

    add wish input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,36 @@
 import { Component } from '@angular/core';
 import { WishItem } from '../shared/models/wishItem';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   items: WishItem[] = [
-    new WishItem('To Learn Angular'),
-    new WishItem('Go grocery shopping', true),
-    new WishItem('Make dinner')
+    // new WishItem('To Learn Angular'),
+    // new WishItem('Go grocery shopping', true),
+    // new WishItem('Make dinner')
   ]
+  
+  newWishText = '';
+
   title = 'wishlist';
+
+  addNewWish() {
+    //add new wish to the items array
+    //clear textbox
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = '';
+
+  }
 
   toggleItem(item : WishItem) {
     item.isComplete = !item.isComplete;
